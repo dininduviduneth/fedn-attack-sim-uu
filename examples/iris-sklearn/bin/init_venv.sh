95eddfbc5056ce7dbbddba6868e666ea25c3fa2e
Dinindu Seneviratne <dininduviduneth@gmail.com>
2024-04-09 18:09:35 +0200

    Implement train and validation of iris model

--- examples/iris-sklearn/bin/init_venv.sh
+++ examples/iris-sklearn/bin/init_venv.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Init venv
-python3 -m venv .mnist-pytorch
+python3 -m venv .iris-sklearn
 
 # Pip deps
 .iris-sklearn/bin/pip install --upgrade pip
-sudo .mnist-pytorch/bin/pip install -e ../../fedn
+sudo .iris-sklearn/bin/pip install -e ../../fedn
 .iris-sklearn/bin/pip install -r requirements.txt
