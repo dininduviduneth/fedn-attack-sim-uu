1feac803d096fc5634350e2d037e70a5888539c3
Dinindu Seneviratne <dininduviduneth@gmail.com>
2024-04-10 15:55:02 +0200

    Add start_client scripts :zap:

--- examples/iris-sklearn/bin/start_clients.sh
+++ examples/iris-sklearn/bin/start_clients.sh
@@ -23,6 +23,8 @@ if ! [[ "$malicious_client_count" =~ ^[0-9]+$ ]]; then
     exit 1
 fi
 
+docker build -t iris-sklearn .
+
 # Loop for count of clients if benign_client_count is greater than 0
 if [ "$benign_client_count" -gt 0 ]; then
     for i in $(seq 1 "$benign_client_count"); do
@@ -30,11 +32,11 @@ if [ "$benign_client_count" -gt 0 ]; then
         docker run -d \
         -v $PWD/client.yaml:/app/client.yaml \
         -v $PWD/data/clients/$i:/var/data \
-        -e ENTRYPOINT_OPTS="--data_path=/var/data/mnist.pt" \
+        -e ENTRYPOINT_OPTS="--data_path=/var/data/iris.json" \
         --add-host=api-server:"$combiner_ip" \
         --add-host=combiner:"$combiner_ip" \
         --name benign_client$i \
-        ghcr.io/scaleoutsystems/fedn/fedn:master-mnist-pytorch run client -in client.yaml --name benign_client$i
+        iris-sklearn:latest run client -in client.yaml --name benign_client$i
     done
 fi
 
@@ -46,10 +48,10 @@ if [ "$malicious_client_count" -gt 0 ]; then
         docker run -d \
         -v $PWD/client.yaml:/app/client.yaml \
         -v $PWD/data/clients/$client_number:/var/data \
-        -e ENTRYPOINT_OPTS="--data_path=/var/data/mnist.pt --malicious=True --attack=$attack_type" \
+        -e ENTRYPOINT_OPTS="--data_path=/var/data/iris.json --malicious=True --attack=$attack_type" \
         --add-host=api-server:"$combiner_ip" \
         --add-host=combiner:"$combiner_ip" \
         --name malicious_client$i \
-        ghcr.io/scaleoutsystems/fedn/fedn:master-mnist-pytorch run client -in client.yaml --name malicious_client$i
+        iris-sklearn:latest run client -in client.yaml --name malicious_client$i
     done
 fi
