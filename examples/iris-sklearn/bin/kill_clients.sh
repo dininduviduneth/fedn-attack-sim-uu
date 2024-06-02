90bcf8574f5cb422ed9258c4c64623a51c6d621a
Dinindu Seneviratne <dininduviduneth@gmail.com>
2024-06-02 06:11:04 +0000

    Minor additions to read parameters

--- examples/iris-sklearn/bin/kill_clients.sh
+++ examples/iris-sklearn/bin/kill_clients.sh
@@ -1,3 +1,4 @@
 docker kill $(docker ps -aq --filter "name=client")
 docker rm $(docker ps -aq --filter "name=client")
 docker rmi $(docker images -aq --filter "reference=iris-sklearn:*")
+sudo rm -rf data/clients/
--- examples/iris-sklearn/bin/run_entrypoint.sh
+++ examples/iris-sklearn/bin/run_entrypoint.sh
@@ -1,3 +1,3 @@
 python client/entrypoint init_seed
-python client/entrypoint train --in_model_path=seed.npz --out_model_path=trained_model.npz --data_path=iris_data/iris.json
-python client/entrypoint validate --in_model_path=trained_model.npz --out_json_path=metrics.json --data_path=iris_data/iris.json
+sudo client/entrypoint train --in_model_path=seed.npz --out_model_path=trained_model.npz --data_path=iris_data/iris.json
+sudo client/entrypoint validate --in_model_path=trained_model.npz --out_json_path=metrics.json --data_path=iris_data/iris.json
